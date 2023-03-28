b2aa2a4568c2ec581df117442e594fc24d6a1097
sheshu2906 <sheshnath.yadav@knoldus.com>
2023-03-28 15:36:24 +0530

    files updated

--- Fibonacci/src/main/scala/Fibonacci.scala
+++ Fibonacci/src/main/scala/Fibonacci.scala
@@ -2,14 +2,13 @@ package com.knoldus
 
 import scala.annotation.tailrec
 
-
 class Fibonacci {
 
   @tailrec
   final def getFibonacciValue(number: Int, firstValue: Int = 0, secondValue: Int = 1): Int = {
 
-    if(number<0) throw new IllegalArgumentException("Negative value Found")
-    if(number == 0) firstValue
+    if (number < 0) throw new IllegalArgumentException("Negative value Found")
+    if (number == 0) firstValue
     else if (number == 1) secondValue
     else
       getFibonacciValue(number - 1, secondValue, firstValue + secondValue)
